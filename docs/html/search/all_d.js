5f52030d8e8e0b1a221a886877b307a807b37968
edurs2602 <edurs.2602@gmail.com>
2023-07-06 14:18:52 -0300

    feat: documentação, pdf atualizado e readme atualizados

--- docs/html/search/all_d.js
+++ docs/html/search/all_d.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['usuario_0',['Usuario',['../classUsuario.html',1,'Usuario'],['../classUsuario.html#aa85a5371a098dfba5449140d9b8a472f',1,'Usuario::Usuario()'],['../classUsuario.html#a6653956a32794c3ea4b6e0e566b11aea',1,'Usuario::Usuario(std::string email, std::string senha, std::string nome)']]]
+  ['upload_0',['upload',['../classSistema.html#afb714b0275f5d6c0523a8884639ddbce',1,'Sistema']]],
+  ['usuario_1',['Usuario',['../classUsuario.html',1,'Usuario'],['../classUsuario.html#aa85a5371a098dfba5449140d9b8a472f',1,'Usuario::Usuario()'],['../classUsuario.html#a6653956a32794c3ea4b6e0e566b11aea',1,'Usuario::Usuario(std::string email, std::string senha, std::string nome)']]]
 ];
